000742a091973c3c1ca39e7c6490027d66ec6ab5
josephttran <42635012+josephttran@users.noreply.github.com>
2018-11-16 16:05:21 -0800

    add import

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
 
 import { Hero } from './hero';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InMemoryDataService {
-
-  constructor() { }
-
+export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
       { id: 11, name: 'Mr. Nice' },
